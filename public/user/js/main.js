f31c45c3445db717a19b7b1e72a1501a96d5cc75
Akash Kinwad <akashkinwad@gmail.com>
2022-11-14 13:30:26 +0530

    nft-mint function call

--- public/user/js/main.js
+++ public/user/js/main.js
@@ -1,17 +1,17 @@
 // =====Price slider js=====
-$(function() {
-	$( "#slider-range" ).slider({
-	  range: true,
-	  min: 0,
-	  max: 500,
-	  values: [ 0, 500 ],
-	  slide: function( event, ui ) {
-		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-	  }
-	});
-	$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-	  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-});
+// $(function() {
+// 	$( "#slider-range" ).slider({
+// 	  range: true,
+// 	  min: 0,
+// 	  max: 500,
+// 	  values: [ 0, 500 ],
+// 	  slide: function( event, ui ) {
+// 		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+// 	  }
+// 	});
+// 	$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+// 	  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+// });
 
 // =====file upload js=====
 function readURL(input) {
