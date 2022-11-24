a3ca3985fe61ef4271613f5ce4fc8a5a0bb88e36
Akash Kinwad <akashkinwad@gmail.com>
2022-11-24 22:12:22 +0530

    Corrections

--- app/models/nft_post.rb
+++ app/models/nft_post.rb
@@ -1,5 +1,5 @@
 class NftPost < ApplicationRecord
-  validates :name, :description, :listing_price, presence: true
+  validates :title, :description, :listing_price, presence: true
   validates :attachment_url, presence: true, on: :update
 
   belongs_to :user
