f556fb982f3af49b8ee22196d3a6a36713da6369
Akash Kinwad <akashkinwad@gmail.com>
2023-01-21 12:04:17 +0530

    deployment script changes

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,11 @@
 echo Hello, which branch you would like to deply?
 read branch_name
 
-if [ -z "$branch_name" ]
-then
-  branch_name=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-  echo Default branch is $branch_name
-  echo "\$branch_name is empty"
+if [ -n "$branch_name" ]; then
+  echo ""
 else
-  echo $branch_name
-  echo "\$branch_name is NOT empty"
+  cd /var/www/talent/code
+  branch_name=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 fi
+
+echo Branch is $branch_name
--- server-deploy.sh
+++ server-deploy.sh
@@ -17,3 +17,28 @@ RAILS_ENV=production bundle exec rails assets:precompile
 passenger-config restart-app .
 # rvmsudo passenger-config restart-app
 echo Deployment Completed! Successfully deployed $branch_name
+
+# -----------------------------------------------------------------------------
+# v2
+
+#!/bin/bash
+cd /var/www/talent/code
+
+echo Hello, which branch you would like to deply?
+read branch_name
+
+if [ -n "$branch_name" ]; then
+  echo Deploying $branch_name, please wait.....
+else
+  branch_name=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+  echo Deploying current branch $branch_name, please wait.....
+fi
+
+echo
+git fetch origin
+git checkout $branch_name
+git pull origin $branch_name
+RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails assets:precompile
+passenger-config restart-app .
+echo Deployment Completed! Successfully deployed $branch_name
