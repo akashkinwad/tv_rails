5d2819d985c131c25a6c8a056d7c4b3146dd601f
Akash Kinwad <akashkinwad@gmail.com>
2023-02-21 17:03:04 +0530

    Setup issue

--- app/models/sign_nft_request.rb
+++ app/models/sign_nft_request.rb
@@ -1,5 +1,5 @@
 class SignNftRequest < ApplicationRecord
-  before_create :increment_count
+  before_create :increment_count, unless: :skip_callbacks
 
   def increment_count
     self.count = SignNftRequest.maximum(:count).next
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+SignNftRequest.create({ skip_callbacks: true })
